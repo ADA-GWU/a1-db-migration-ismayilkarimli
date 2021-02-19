2008856d19c42de128baec680c34144c9c011468
Ismayil <ikarimli4586@ada.edu.az>
2021-02-19 07:43:23 +0000

    added sql query to create db

--- README.md
+++ README.md
@@ -7,7 +7,6 @@ Two simple bash scripts for migrating the database to a new state and rolling it
 * Linux distro or MacOS
 
 ## Installation
-
 Use the following commands to download and set up the scripts:
 ```
 git clone https://github.com/ADA-GWU/a1-db-migration-ismayilkarimli.git $HOME/a1-db-migration-ismayilkarimli
@@ -25,5 +24,10 @@ $HOME/a1-db-migration-ismayilkarimli/rollback.sh [database name]
 ```
 ### Example:
 ```bash
-$HOME/a1-db-migration-ismayilkarimli/migration.sh assignment01
+$HOME/a1-db-migration-ismayilkarimli/migration.sh db4586
+```
+#### Note:
+If you do not have the required database, you can create it by running the following command (ensure that there is no database with name db4586):
+```bash
+psql -U postgres -f $HOME/a1-db-migration-ismayilkarimli/create_db.sql
 ```
---
+++ create_db.sql
@@ -0,0 +1,26 @@
+create database db4586;
+begin;
+\c db4586;
+drop table if exists students;
+drop table if exists interests;
+create table if not exists students(
+    st_id int,
+    st_name varchar(15),
+    st_last varchar(15)
+);
+create table if not exists interests(
+    student_id int,
+    interest text
+);
+insert into students values(1, 'Walter', 'White');
+insert into students values(2, 'Beth', 'Harmon');
+insert into students values(3, 'Elliot', 'Alderson');
+insert into interests values(1, 'Chemistry');
+insert into interests values(1, 'Cooking');
+insert into interests values(2, 'Chess');
+insert into interests values(2, 'Travel');
+insert into interests values(2, 'Reading');
+insert into interests values(3, 'Computers');
+insert into interests values(3, 'Hacking');
+insert into interests values(3, 'Programming');
+commit;
